05fc0103f8257a00f9b76b9993b778a41b3ce74a
Nick J Moss <78372873+nickjmoss@users.noreply.github.com>
2023-02-02 23:20:24 -0700

    app is ready for development

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import styles from './app.scss';
 import classNames from 'classnames/bind';
 import { ConfigProvider, Button, message } from 'antd';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginRequired from '@components/Login/LoginRequired/LoginRequired';
 import Main from '@components/Main/main';
+import styles from './app.scss';
 
 const cx = classNames.bind(styles);
 
@@ -15,33 +15,33 @@ const App = () => {
 	return (
 		<ConfigProvider theme={
 			{
-				"token": {
-				  "colorPrimary": "#585de4",
-				  "colorSuccess": "#00c371",
-				  "colorWarning": "#f8c220",
-				  "colorError": "#f14035",
-				  "colorInfo": "#14a6f8",
-				  "borderRadius": 6,
-				  "wireframe": false
-				}
-			  }
+				'token': {
+					'colorPrimary': '#585de4',
+					'colorSuccess': '#00c371',
+					'colorWarning': '#f8c220',
+					'colorError': '#f14035',
+					'colorInfo': '#14a6f8',
+					'borderRadius': 6,
+					'wireframe': false,
+				},
+			}
 		}
 		>
 			<BrowserRouter>
 				<Routes>
-					<Route exact path='/login' element={<div className={cx('test')}>Login Page</div>}/>
-					<Route path='/logout' element={<Button type='default' onClick={() => message.info('Yay')}>Logout</Button>}/>
+					<Route exact path="/login" element={<div className={cx('test')}>Login Page</div>}/>
+					<Route path="/logout" element={<Button type="default" onClick={() => message.info('Yay')}>Logout</Button>}/>
 					<Route path="*"element={
-							<LoginRequired>
-								<Main/>
-							</LoginRequired>
-						}
+						<LoginRequired>
+							<Main/>
+						</LoginRequired>
+					}
 					/>
 				</Routes>
 			</BrowserRouter>
 		</ConfigProvider>
-	)
-}
+	);
+};
 
 const reactRoot = ReactDOM.createRoot(root);
-reactRoot.render(<App/>)
+reactRoot.render(<App/>);
