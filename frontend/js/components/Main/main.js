2d6b6c9ce1ca9ed8bae48e56d864107974c14629
Nick J Moss <78372873+nickjmoss@users.noreply.github.com>
2023-02-23 14:27:22 -0700

    menu bar doen

--- frontend/js/components/Main/main.js
+++ frontend/js/components/Main/main.js
@@ -1,16 +1,55 @@
-import { Button } from 'antd';
+import { Button, Input } from 'antd';
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Routes, Route, useNavigate, Link } from 'react-router-dom';
+import styles from './main.scss';
+import classNames from 'classnames/bind';
 import { useStores } from '@stores';
+import SVG from '@reusableComponents/SVG/svg';
+import { UserOutlined, SearchOutlined } from '@ant-design/icons';
+import TabLink from '@reusableComponents/TabLink/tabLink';
+
+const cx = classNames.bind(styles);
 
 const Main = () => {
-	const { UserStore } = useStores();
 	const navigate = useNavigate();
+	const { UserStore } = useStores();
 	return (
-		<>
-			<div>Main Page</div>
-			<Button type="default" onClick={() => UserStore.logoutUser(navigate)}>Log Out</Button>
-		</>
+		<div className={cx('main')}>
+			<div className={cx('header-container')}>
+				<div>
+					<Link to="/"><SVG name="wishSuitePrimary" /></Link>
+				</div>
+				<div className={cx('menu-buttons')}>
+					<div className={cx('menu-button')}>
+						<TabLink to="/">Home</TabLink>
+					</div>
+					<div className={cx('menu-button')}>
+						<TabLink to="/wishlists">Wishlists</TabLink>
+					</div>
+					<div className={cx('menu-button')}>
+						<TabLink to="/friends">Friends</TabLink>
+					</div>
+				</div>
+				<div className={cx('search-profile')}>
+					<div className={cx('search')}>
+						<Input.Search size="medium" className={cx('search-bar')} enterButton={<SearchOutlined/>} placeholder="Search for an item..." />
+					</div>
+					<div className={cx('profile')}>
+						<TabLink to="/profile">
+							<UserOutlined className={cx('user-icon')}/>
+							<span style={{ paddingLeft: '10px' }}>Profile</span>
+						</TabLink>
+					</div>
+				</div>
+			</div>
+			<Routes>
+				<Route path="/" element={<div>Home</div>}/>
+				<Route path="/search" element={<div>Search Results</div>}/>
+				<Route path="/profile" element={<div>Profile Page<Button type="default" onClick={() => UserStore.logoutUser(navigate)}>Log Out</Button></div>}/>
+				<Route path="/wishlists" element={<div>Wishlists Page</div>}/>
+				<Route path="/friends" element={<div>Friends Page</div>}/>
+			</Routes>
+		</div>
 	);
 };
 
